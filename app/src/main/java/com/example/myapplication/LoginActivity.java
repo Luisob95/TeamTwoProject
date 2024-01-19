1ff34a023a39c22d642e6b777a62992524157e03
Shane Bridges <SMBridges@student.fullsail.edu>
2024-01-18 23:32:52 -0600

    Login Page with temporary functionality Log In Info User: Admin Pass: Admin
    To-Do:
    Move Login functionality to LoginActivity

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+/*
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_login);
+        Button loginBtn = findViewById(R.id.btnLogin);
+        EditText usernameEditText = findViewById(R.id.username_EditText);
+        EditText passwordEditText = findViewById(R.id.password_EditText);
+
+        loginBtn.setOnClickListener(v -> {
+            if("admin".equals(usernameEditText.getText().toString()) &&
+               "admin".equals(passwordEditText.getText().toString())){
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+
+}
+
+*/
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        {
+            setContentView(R.layout.page_login);
+            Button loginBtn = findViewById(R.id.btnLogin);
+            EditText usernameEditText = findViewById(R.id.username_EditText);
+            EditText passwordEditText = findViewById(R.id.password_EditText);
+            loginBtn.setOnClickListener(v -> {
+                if ("admin".equals(usernameEditText.getText().toString()) &&
+                        "admin".equals(passwordEditText.getText().toString())) {
+                    setContentView(R.layout.page_home);
+                }
+                else {}
+            });
+            /*
+            boolean tester = false;
+            if (!tester) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+        setContentView(R.layout.page_home);
+        */
+
+        }
     }
 }
