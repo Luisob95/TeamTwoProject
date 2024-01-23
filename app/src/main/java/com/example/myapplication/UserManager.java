1135b1c353866924a0ade120d8c88fc52b00b154
Shane Bridges <SMBridges@student.fullsail.edu>
2024-01-23 12:58:00 -0600

    Broke Just incase I break it more Revert too
    Change Icons to svg files

--- app/src/main/java/com/example/myapplication/UserManager.java
+++ app/src/main/java/com/example/myapplication/UserManager.java
@@ -2,11 +2,21 @@ package com.example.myapplication;
 
 public class UserManager {
     private static boolean userLoggedIn = true;
+    private static Object[][] userStats= {
+            {"Push-ups", 25},
+            {"Calories", 34},
+            {"Sit-ups", 23},
+            {"Breaks", 5},
+            {"Meditation",3}
+    };
     public static boolean isUserLoggedIn(){
         return userLoggedIn;
     }
     public static void setUserLoggedIn(boolean  loggedIn)
     {
         userLoggedIn = loggedIn;
+    }
+    public static Object[][] getCurrentStat(){
+        return userStats;
     }
 }
