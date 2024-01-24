89da0b6ba3148ae39362960e1f4e299d5103e5ed
Shane Bridges <SMBridges@student.fullsail.edu>
2024-01-24 15:31:12 -0600

    Rough Draft of Randomize UI complete
    Need to move it to another activity
    Next Going to Work on How the file will save or transfer to the generator
    Also need to add and error checker for the input so it wont break the Generator

--- app/src/main/java/com/example/myapplication/PlanActivity.java
+++ app/src/main/java/com/example/myapplication/PlanActivity.java
@@ -2,12 +2,14 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.Layout;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
@@ -19,30 +21,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintSet;
 
 public class PlanActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set Layout
         setContentView(R.layout.page_plan_menu);
-
+        // Randomize Navigation
         TextView btnRandomize = findViewById(R.id.btn_Text_Randomizer);
         btnRandomize.setOnClickListener(v -> {
-            setContentView(R.layout.page_plan_randomizer);
-            LinearLayout layoutDifficulty = findViewById(R.id.linearLayout);
-            createSpinnerButton(layoutDifficulty);
+            setContentView(R.layout.page_plan_randomizer); //Need to make this an intent to switch activity and move all randomize functional
+            // Initialization
             ToggleButton toggleButton = findViewById(R.id.toggle_btn);
+            LinearLayout dynamicLayout = findViewById(R.id.linearLayout);
+            // Create Buttons For All Toggle
+            createDifficultyBtn(dynamicLayout);
+            // Toggle Button Functionality
             toggleButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
-                layoutDifficulty.removeAllViews();
+                // Delete Current Dynamic Buttons
+                dynamicLayout.removeAllViews();
+                // If All
                 if (isChecked) {
-                    createSpinnerButton(layoutDifficulty);
-
-                } else {
-
+                    createDifficultyBtn(dynamicLayout);
+                }
+                // If Exercise
+                else {
+                    createTimeSettingsButton(dynamicLayout);
                 }
             });
-
         });
-
         // Menu Navigation
         ImageButton btnHome = findViewById(R.id.home_icon);
         ImageButton btnProfile = findViewById(R.id.icon_Profile);
@@ -55,22 +61,40 @@ public class PlanActivity extends AppCompatActivity {
             startActivity(intent);
         });
     }
-    // Difficulty
-    public void createSpinnerButton(LinearLayout btnLayout) {
+    public void createDifficultyBtn(LinearLayout btnLayout) {
+        // Difficulty Label
         TextView labelDifficulty = new TextView(this);
         labelDifficulty.setText("Difficulty");
+        labelDifficulty.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        btnLayout.addView(labelDifficulty);
+        // Drop Down Button
         Spinner spnDifficulty = new Spinner(this);
         String[] items = {"Easy", "Medium", "Hard", "Nightmare"};
-        labelDifficulty.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                R.layout.customspinner, items);
-
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.customspinner, items);
         spnDifficulty.setAdapter(adapter);
         spnDifficulty.setBackgroundResource(R.drawable.rounded_spinner);
-        btnLayout.addView(labelDifficulty);
         btnLayout.addView(spnDifficulty);
     }
-    public void createNumberSliders(LinearLayout btnLayout) {
-
+    public void createTimeSettingsButton(LinearLayout btnLayout){
+        // Label Frequency
+        TextView labelFrequency = new TextView(this);
+        labelFrequency.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        labelFrequency.setText("Frequency");
+        btnLayout.addView(labelFrequency);
+        // Frequency Number Box
+        EditText textFrequency = new EditText(this);
+        textFrequency.setInputType(InputType.TYPE_CLASS_NUMBER);
+        textFrequency.setBackgroundResource(R.drawable.rounded_spinner);
+        btnLayout.addView(textFrequency);
+        // Label Duration
+        TextView labelDuration = new TextView(this);
+        labelDuration.setText("Duration");
+        btnLayout.addView(labelDuration);
+        // Duration Number Box
+        EditText textDuration = new EditText(this);
+        textDuration.setInputType(InputType.TYPE_CLASS_NUMBER);
+        textDuration.setBackgroundResource(R.drawable.rounded_spinner);
+        labelDuration.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        btnLayout.addView(textDuration);
     }
 }
